bc34c57e015360d09ef74b5d86d248eb3b60206f
loveyoubaby5120 <kaipan.1314.666@gmail.com>
2023-01-05 17:12:40 +0800

    fix: test

--- src/lib/typed-env.test.ts
+++ src/lib/typed-env.test.ts
@@ -3,19 +3,19 @@ import { anyEnv, typedEnv } from "./typed-env";
 
 test("boolean env", () => {
   const envBox = typedEnv<ProjectEnvName>("NX_WORKSPACE_ROOT");
-  const rs = envBox.required().toString();
+  const rs = envBox.required().toBoolean();
   console.log("required boolean", rs);
   const envBox2 = anyEnv("ENV_TEXT_BOOLEAN_2");
-  const s = envBox2.optional().toString();
+  const s = envBox2.optional().toBoolean();
   console.log("options boolean", s);
 });
 
 test("integer env", () => {
   const envBox = typedEnv(AllProjectEnvNames.NX_WORKSPACE_ROOT);
-  const rs = envBox.required().toString();
+  const rs = envBox.required().toInt();
   console.log("required integer", rs);
   const envBox2 = anyEnv("ENV_TEST_NUMBER_2");
-  const s = envBox2.optional().toString();
+  const s = envBox2.optional().toInt();
   console.log("options integer", s);
 });
 
@@ -26,13 +26,4 @@ test("string env", () => {
   const envBox2 = anyEnv("ENV_TEST_STRING_2");
   const s = envBox2.optional().toString();
   console.log("options string", s);
-});
-
-test("json env", () => {
-  const envBox = typedEnv<ProjectEnvName>("NX_WORKSPACE_ROOT");
-  const rs = envBox.required().toString();
-  console.log("required json", rs);
-  const envBox2 = anyEnv("ENV_TEST_JSON_2");
-  const s = envBox2.optional().toString();
-  console.log("options json", s);
 });
